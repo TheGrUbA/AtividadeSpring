a789b83ed0151e30ffda7052d5e646efd740e3f7
TheGrUbA <breynerg@gmail.com>
2018-09-08 16:11:21 -0300

    Criando operacao de instanciacao

--- src/main/java/com/beugenio/AtividadeSpring/AtividadeSpringApplication.java
+++ src/main/java/com/beugenio/AtividadeSpring/AtividadeSpringApplication.java
@@ -1,12 +1,27 @@
 package com.beugenio.AtividadeSpring;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.beugenio.AtividadeSpring.domain.Categoria;
+import com.beugenio.AtividadeSpring.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class AtividadeSpringApplication {
+public class AtividadeSpringApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadeSpringApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+ 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
