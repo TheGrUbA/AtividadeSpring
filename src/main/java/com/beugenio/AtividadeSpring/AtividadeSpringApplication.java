cdc2f8c91efc2953a7c7ff75f7b57edc51e9dcac
“Breyner <breynerg@gmail.com>
2018-09-08 15:19:39 -0300

    Criação do Rep

---
+++ src/main/java/com/beugenio/AtividadeSpring/AtividadeSpringApplication.java
@@ -0,0 +1,12 @@
+package com.beugenio.AtividadeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeSpringApplication.class, args);
+	}
+}
