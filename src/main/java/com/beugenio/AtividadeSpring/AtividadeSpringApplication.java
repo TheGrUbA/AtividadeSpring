240465aaae02157089f69767553da28615f05c75
TheGrUbA <breynerg@gmail.com>
2018-09-11 13:43:41 -0300

    Estado e cidade

--- src/main/java/com/beugenio/AtividadeSpring/AtividadeSpringApplication.java
+++ src/main/java/com/beugenio/AtividadeSpring/AtividadeSpringApplication.java
@@ -9,6 +9,10 @@ import com.beugenio.AtividadeSpring.domain.Categoria;
 import com.beugenio.AtividadeSpring.repositories.CategoriaRepository;
 import com.beugenio.AtividadeSpring.domain.Produto;
 import com.beugenio.AtividadeSpring.repositories.ProdutoRepository;
+import com.beugenio.AtividadeSpring.domain.Cidade;
+import com.beugenio.AtividadeSpring.domain.Estado;
+import com.beugenio.AtividadeSpring.repositories.CidadeRepository;
+import com.beugenio.AtividadeSpring.repositories.EstadoRepository;
 
 @SpringBootApplication
 public class AtividadeSpringApplication implements CommandLineRunner {
@@ -17,6 +21,10 @@ public class AtividadeSpringApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadeSpringApplication.class, args);
@@ -31,6 +39,18 @@ public class AtividadeSpringApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+ 		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
---
+++ src/main/java/com/beugenio/AtividadeSpring/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.beugenio.AtividadeSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.beugenio.AtividadeSpring.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
